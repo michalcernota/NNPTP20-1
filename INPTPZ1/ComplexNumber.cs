70d8988a6ce7f7994da4730fa11d9a8dc0ab6380
Michal Černota <st52494@upce.cz>
2020-10-16 22:44:37 +0200

    Code cleanup

---
+++ INPTPZ1/ComplexNumber.cs
@@ -0,0 +1,80 @@
+﻿namespace INPTPZ1
+{
+
+    namespace Mathematics
+    {
+        public class ComplexNumber
+        {
+            public double Real { get; set; }
+            public double Imaginary { get; set; }
+
+            public override bool Equals(object inputObject)
+            {
+                if (inputObject is ComplexNumber inputComplexNumber)
+                {
+                    return inputComplexNumber.Real == Real && inputComplexNumber.Imaginary == Imaginary;
+                }
+                return base.Equals(inputObject);
+            }
+
+            public static readonly ComplexNumber Zero = new ComplexNumber()
+            {
+                Real = 0,
+                Imaginary = 0
+            };
+
+            public ComplexNumber Multiply(ComplexNumber inputNumber)
+            {
+                // Steps:
+                // aRe*bRe + aRe*bIm*i + aIm*bRe*i + aIm*bIm*i*i
+                ComplexNumber thisInstance = this;
+                return new ComplexNumber()
+                {
+                    Real = thisInstance.Real * inputNumber.Real - thisInstance.Imaginary * inputNumber.Imaginary,
+                    Imaginary = thisInstance.Real * inputNumber.Imaginary + thisInstance.Imaginary * inputNumber.Real
+                };
+            }
+
+            public ComplexNumber Add(ComplexNumber InputNumber)
+            {
+                ComplexNumber thisInstance = this;
+                return new ComplexNumber()
+                {
+                    Real = thisInstance.Real + InputNumber.Real,
+                    Imaginary = thisInstance.Imaginary + InputNumber.Imaginary
+                };
+            }
+
+            public ComplexNumber Subtract(ComplexNumber inputNumber)
+            {
+                ComplexNumber thisInstance = this;
+                return new ComplexNumber()
+                {
+                    Real = thisInstance.Real - inputNumber.Real,
+                    Imaginary = thisInstance.Imaginary - inputNumber.Imaginary
+                };
+            }
+
+            public override string ToString()
+            {
+                return $"({Real} + {Imaginary}i)";
+            }
+
+            internal ComplexNumber Divide(ComplexNumber inputNumber)
+            {
+                // Steps:
+                // (aRe + aIm*i) / (bRe + bIm*i)
+                // ((aRe + aIm*i) * (bRe - bIm*i)) / ((bRe + bIm*i) * (bRe - bIm*i))
+                //  bRe*bRe - bIm*bIm*i*i
+                ComplexNumber upperFractionPart = Multiply(new ComplexNumber() { Real = inputNumber.Real, Imaginary = -inputNumber.Imaginary });
+                double lowerFractionPart = (inputNumber.Real * inputNumber.Real) + (inputNumber.Imaginary * inputNumber.Imaginary);
+
+                return new ComplexNumber()
+                {
+                    Real = upperFractionPart.Real / lowerFractionPart,
+                    Imaginary = upperFractionPart.Imaginary / lowerFractionPart
+                };
+            }
+        }
+    }
+}
---
+++ INPTPZ1/Poly.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace INPTPZ1
+{
+    namespace Mathematics
+    {
+        class Poly
+        {
+            public List<ComplexNumber> Coefficients { get; set; }
+
+            public Poly() => Coefficients = new List<ComplexNumber>();
+
+            public Poly Derivate()
+            {
+                Poly poly = new Poly();
+                for (int i = 1; i < Coefficients.Count; i++)
+                {
+                    poly.Coefficients.Add(Coefficients[i].Multiply(new ComplexNumber() { Real = i }));
+                }
+
+                return poly;
+            }
+
+            public ComplexNumber Evaluate(ComplexNumber inputNumber)
+            {
+                ComplexNumber result = ComplexNumber.Zero;
+                for (int i = 0; i < Coefficients.Count; i++)
+                {
+                    ComplexNumber currentCoefficient = Coefficients[i];
+                    ComplexNumber powerResult = inputNumber;
+                    int power = i;
+
+                    if (i > 0)
+                    {
+                        for (int j = 0; j < power - 1; j++)
+                        {
+                            powerResult = powerResult.Multiply(inputNumber);
+                        }
+
+                        currentCoefficient = currentCoefficient.Multiply(powerResult);
+                    }
+
+                    result = result.Add(currentCoefficient);
+                }
+
+                return result;
+            }
+
+            public override string ToString()
+            {
+                string result = "";
+                for (int i = 0; i < Coefficients.Count; i++)
+                {
+                    result += Coefficients[i];
+                    if (i > 0)
+                    {
+                        for (int j = 0; j < i; j++)
+                        {
+                            result += "x";
+                        }
+                    }
+                    result += " + ";
+                }
+                return result;
+            }
+        }
+    }
+}
